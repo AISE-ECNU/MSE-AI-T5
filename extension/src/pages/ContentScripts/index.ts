5f49ddddfac120b1c6d1615c1564468f973a427b
lyf <1141730046@qq.com>
2025-04-20 19:43:11 +0800

    feat: Redesign product entry layout.

--- extension/src/pages/ContentScripts/index.ts
+++ extension/src/pages/ContentScripts/index.ts
@@ -1,8 +1,3 @@
 import "./index.scss";
 
-// import "./features/test1";
-// import "./features/test2";
-
-import "./features/three-indicators";
-// import "./features/three-indicators/index";
 import "./features/analysis";
